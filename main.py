aec90fad5b37a1a65ec04c8f285eda15ff2d6c02
Rohan Dugad <rohand2468@gmail.com>
2021-01-25 23:43:28 -0500

    Sent and received one char over serial

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import serial
 ser = serial.Serial('/dev/ttyS0', 115200)
 
-message = '0'
+message = '1'
 message = message.encode()
 
 while True:
     ser.write(message)
+
+    receive = ser.read().decode()
+    print(receive)
