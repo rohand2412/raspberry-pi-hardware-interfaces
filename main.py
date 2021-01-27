ecee003497e0582f8439f911fd474c623bb8df37
Rohan Dugad <rohand2468@gmail.com>
2021-01-27 17:22:31 -0500

    Implemented state machine and packets

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import numpy as np
 from serialwrapper import SerialWrapper
 
 SerialWrapper.begin('/dev/ttyS0', 115200)
 
-message = 'a'
+message = [0, 1, 2, 3, 4, 0xff, 0xfe, 5, 6, 8]
+packet = np.zeros(10, dtype=np.uint8)
 
 while True:
-    SerialWrapper.send([message])
+    SerialWrapper.send(message)
 
-    receive = SerialWrapper.receive()
-    print(receive)
+    receive = SerialWrapper.receive(packet)
+    if type(receive) == np.ndarray:
+        print(receive)
