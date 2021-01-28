b214dfd891ceb34aa5b223e06c8790a51379a201
Rohan Dugad <rohand2468@gmail.com>
2021-01-28 00:11:05 -0500

    Implemented dynamic receive packet

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from serialwrapper import SerialWrapper
 SerialWrapper.begin('/dev/ttyS0', 115200)
 
 message = [0, 1, 2, 3, 4, 0x1f, 0x1e, 5, 6, 8]
-packet = np.zeros(10, dtype=np.uint8)
+packet = np.zeros(256, dtype=np.uint8)
 
 while True:
     SerialWrapper.send(message)
 
-    receive = SerialWrapper.receive(packet)
+    receive, itemNum = SerialWrapper.receive(packet)
     if type(receive) == np.ndarray:
-        print(receive)
+        print(receive[:itemNum])
