206d76aa28bc1caf74ed5525a54f4e60621f3a68
Rohan Dugad <rohand2468@gmail.com>
2021-01-27 23:57:29 -0500

    Implemented CRC corruption checking

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from serialwrapper import SerialWrapper
 
 SerialWrapper.begin('/dev/ttyS0', 115200)
 
-message = [0, 1, 2, 3, 4, 0xff, 0xfe, 5, 6, 8]
+message = [0, 1, 2, 3, 4, 0x1f, 0x1e, 5, 6, 8]
 packet = np.zeros(10, dtype=np.uint8)
 
 while True:
