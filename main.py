d0cac9dedf2d18798a51e0863e707249cad6882f
Rohan Dugad <rohand2468@gmail.com>
2021-01-28 09:25:43 -0500

    Cleaned up code

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
+#!/usr/bin/env python3
+"""Example code for serialwrapper library"""
+
 import numpy as np
 from serialwrapper import SerialWrapper
 
-SerialWrapper.begin('/dev/ttyS0', 115200)
+def main():
+    """Demonstrates correct usage of serialwrapper library"""
+    SerialWrapper.begin('/dev/ttyS0', 115200)
 
-message = [0, 1, 2, 3, 4, 0x1f, 0x1e, 5, 6, 8]
-packet = np.zeros(256, dtype=np.uint8)
+    message = [0, 1, 2, 3, 4, 0x1f, 0x1e, 5, 6, 8]
+    packet = np.zeros(256, dtype=np.uint8)
 
-while True:
-    SerialWrapper.send(message)
+    while True:
+        SerialWrapper.send(message)
 
-    receive, itemNum = SerialWrapper.receive(packet)
-    if type(receive) == np.ndarray:
-        print(receive[:itemNum])
+        receive, item_num = SerialWrapper.receive(packet)
+        if isinstance(receive, np.ndarray):
+            print(receive[:item_num])
+
+if __name__ == '__main__':
+    main()
